a93e6632bf6efc697aebc778001f23a8f424f693
Zozoom <kiss.zoltan.pti@gmail.com>
2018-01-01 20:23:44 +0100

    Next commit

--- loggerPy.py
+++ loggerPy.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
-
-import os, errno
+#!/usr/bin/python
 import time
 import logging
+from isFileExist import folderIsExist
 
 #================================================================================#
 # Variables
@@ -10,28 +9,27 @@ defaultMsg = ''
 directory = 'logs/'
 logFileName = 'tempLog_'+time.strftime('%Y_%m_%d')+'.log'
 wasInit = False
+
 #================================================================================#
 # Get Actual Time in String
 def getTimeInStr():
-    return str(time.strftime('%Y/%m/%d  %H:%M:%S')) 
+    return str(time.strftime('%Y/%m/%d  %H:%M:%S'))
+
 #================================================================================#
 #Log Init
 def log_init():
-    if not os.path.exists(directory):
-        try:
-            os.makedirs(directory)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
+
+    folderIsExist(directory)
     
-    LOG_FILENAME = directory+logFileName
-    logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO)
+    log_filename = directory+logFileName
+    logging.basicConfig(filename=log_filename,level=logging.INFO)
     logging.info('\n')
     logging.info(' ********* Starting at '+time.strftime('%Y/%m/%d  %H:%M:%S')+' *********')
 
     defaultMsg = getTimeInStr()+': Logger was inicialized.'   
     print (defaultMsg)
     logging.info(defaultMsg)
+
     global wasInit
     wasInit = True
     
@@ -40,8 +38,7 @@ def log_init():
 def logger(message,level):
     if(not wasInit):
         log_init()
-    defaultMsg = getTimeInStr()+': '+level+' > '+message   
+
+    defaultMsg = getTimeInStr()+': ['+level+'] > '+message+' <'
     print (defaultMsg)
     logging.info(defaultMsg)
- 
- 
